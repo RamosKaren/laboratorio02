c4f1537eaf7d0797d60ff26689c3ed6ccfff546c
Karen Ramos <karenramos2006.18@gmail.com>
2026-04-18 16:02:59 -0600

    Archivos laboratorio

---
+++ Laboratorio2_sramos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2_sramos;
+
+/**
+ *
+ * @author karen
+ */
+public class Laboratorio2_sramos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
