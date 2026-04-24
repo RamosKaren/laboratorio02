2140b388850b71c14882f4d141b61a0db91d7e39
karen <karen@Sofia>
2026-04-24 01:09:07 -0600

    Implemente diseño de interfaces y diseño

---
+++ src/main/java/com/mycompany/laboratorio2_sramos/Laboratorio2_sramos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2_sramos;
+
+/**
+ *
+ * @author karen
+ */
+public class Laboratorio2_sramos {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+      new Login().setVisible(true);
+        });
+        }
+}
